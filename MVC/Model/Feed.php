1633c84f1a614e4f338d7c6801e4e0ba2538d394
___hanzel___ <77740697+Sabotron@users.noreply.github.com>
2022-04-29 15:15:07 -0600

    Add files via upload

--- MVC/Model/Feed.php
+++ MVC/Model/Feed.php
@@ -7,17 +7,23 @@ class Feed extends Conexion
     private $strTitle;
     private $strDescription;
     private $strImage;
-    
-    
+
+
     function getUserFeeds()
-{
-   $intUid = $_SESSION['user']['id'];
-   $sql = "SELECT * FROM feed WHERE userId = ?";
-   $result = $this->connect()->prepare($sql);
-   $result->execute([$intUid]);
-   return $result;
-}
-}
+    {
+        $intUid = $_SESSION['user']['id'];
+        $sql = "SELECT * FROM feed WHERE userId = ?";
+        $result = $this->connect()->prepare($sql);
+        $result->execute([$intUid]);
+        return $result;
+    }
 
-
-?>
+    function getUserFilter($categoryId)
+    {
+        $intUid = $_SESSION['user']['id'];
+        $sql = "SELECT * FROM feed WHERE userId = ? AND categoryId = ?";
+        $result = $this->connect()->prepare($sql);
+        $result->execute([$intUid, $categoryId]);
+        return $result;
+    }
+}
